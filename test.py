a8f4a665110ebbcc2b2f8fd2edcb7dea847c3c67
FreezeZeni <markermohin@gmail.com>
2025-04-03 22:48:34 +0300

    0.7.2

--- test.py
+++ test.py
@@ -1,31 +1,26 @@
 import win32con
 import win32api
 import time
-import ctypes
-from pynput.mouse import Controller, Button, Listener
-from pynput import mouse, keyboard
+from pynput.mouse import Controller, Button
+from pynput import mouse
 
 
 mouse = Controller()
 
 # cps = int(input())
-cps = 50
+cps = 15
 interval = 1 / cps
 
 def is_caps_lock_on():
     return win32api.GetKeyState(win32con.VK_CAPITAL) & 0x0001 != 0
 
-def is_mouse_clicked():
-    return ctypes.windll.user32.GetAsyncKeyState(5) != 0
-
-
 while True:
-    if is_mouse_clicked():
+    if is_caps_lock_on():
         start_time = time.perf_counter()
-        while is_mouse_clicked():
+        while is_caps_lock_on():
             current_time = time.perf_counter()
             if current_time - start_time >= interval:
-                mouse.click(Button.right)
+                mouse.click(Button.left)
                 start_time = current_time
     else:
         time.sleep(0.3)
